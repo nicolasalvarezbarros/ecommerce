d83f8da72a1b485ced4496ae44d996207cee2f4e
nicolasalvarezbarros <88471038+nicolasalvarezbarros@users.noreply.github.com>
2021-08-26 11:29:13 -0300

    prueba desafiate1

--- js/login.js
+++ js/login.js
@@ -15,14 +15,29 @@ const buttonSingIn = () => {
       }  
 };
 
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+  }
+
 // Autentificación con google
-const onSignIn = (googleUser) => {
+/*const onSignIn = (googleUser) => {
     var profile = googleUser.getBasicProfile();
     localStorage.setItem("datos", profile.getEmail());
     window.location="index.html";
 };
 //
-
+*/
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
