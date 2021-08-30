6c126cff046c78299f7bdd0c1ee88e26ea789737
nicolasalvarezbarros <88471038+nicolasalvarezbarros@users.noreply.github.com>
2021-08-30 11:44:47 -0300

    desafiate 1 final

--- js/auxlogin.js
+++ js/auxlogin.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(e){
 //    console.log(JSON.parse(localStorage.getItem("datos")));
-    if ((JSON.parse(localStorage.getItem("datos")) === null) || (localStorage.getItem("datos2") === null)) {
-      window.location="login.html";
-    } 
+    if (JSON.parse(localStorage.getItem("datos")) === null) {
+      if (localStorage.getItem("datos2") === null) {
+        window.location="login.html";
+      };
+//      window.location="login.html";
+    };
   });
