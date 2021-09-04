611e506c48f8f55e8fee9fae58e1bc0aac473308
nicolasalvarezbarros <88471038+nicolasalvarezbarros@users.noreply.github.com>
2021-09-03 23:14:11 -0300

    Update auxlogin.js

--- js/auxlogin.js
+++ js/auxlogin.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(e){
 //    console.log(JSON.parse(localStorage.getItem("datos")));
     if (JSON.parse(localStorage.getItem("datos")) === null) {
-      window.location="login.html";
+      // Autentificación con google
+      if (localStorage.getItem("datos2") === null) {
+        window.location="login.html";
+      };
+//      window.location="login.html";
     };
   });
