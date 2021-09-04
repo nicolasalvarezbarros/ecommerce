a574a814f301dba37b8fbbd3a1ef37d5b6a2a426
nicolasalvarezbarros <88471038+nicolasalvarezbarros@users.noreply.github.com>
2021-09-04 19:45:40 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -42,7 +42,6 @@ var getJSONData = function(url){
 
 // cerrar sesión de google.
 function signOut() {
-  GoogleAuth.signOut ()
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
