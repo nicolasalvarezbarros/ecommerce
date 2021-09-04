215bb1474db4d92f148a4ea11c6ce81b1e4d371a
nicolasalvarezbarros <88471038+nicolasalvarezbarros@users.noreply.github.com>
2021-09-04 17:44:31 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -46,8 +46,9 @@ function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
+    location.reload();
   });
-  gapi.auth2.getAuthInstance().disconnect();
+  //gapi.auth2.getAuthInstance().disconnect();
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
